8d271b6c3983534797385d255bc5dcd16d266545
David Lam <david@buddybuild.com>
2017-03-30 14:23:36 -0700

    Update framework

--- RomanNumeralCalculator/AppDelegate.swift
+++ RomanNumeralCalculator/AppDelegate.swift
@@ -44,7 +44,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
-        BuddyBuildSDK.uiTestRecordingDidReceiveRemoteNotification(userInfo)
+        BuddyBuildSDK.uiTestsDidReceiveRemoteNotification(userInfo)
     }
 }
 
